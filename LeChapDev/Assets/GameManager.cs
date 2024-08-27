f86f4fe1b5f6b365556186f52d764a9d98b99593
CollinWilkerson <wilkerc2@my.erau.edu>
2024-08-27 10:23:36 -0700

    Made Player

---
+++ LeChapDev/Assets/GameManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.Linq;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    [Header("Stats")]
+    public bool gameEnded = false;
+    public float timeToWin; //remaining hat time
+    public float invincibleDuration; //time between hat vulnerability
+    private float hatPickupTime; // time hat was picked up
+
+}
